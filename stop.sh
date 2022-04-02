c05c02f43719a03637b8657d3fc641f43d0f613f
BirdDev <BirdDev@users.noreply.github.com>
2022-04-02 17:25:11 +0200

    Add scripts to start and stop

--- stop.sh
+++ stop.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+BASEDIR=$(dirname "$0")
+
+common_lib_file="$BASEDIR/common.lib.sh"
+if [ -f "$common_lib_file" ]; then
+    . "$common_lib_file"
+else
+    echo "Required file does not exist at '$common_lib_file'." && return 1
+fi
+
 docker-compose down
